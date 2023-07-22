c5993a7973399067993f216858d5fbf5578111f1
anam236razaq <101165022+anam236razaq@users.noreply.github.com>
2023-07-23 02:23:01 +0500

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -32,3 +32,18 @@ valueDisplays.forEach((valueDisplay)=>{
         }
     }, duration)
 });
+
+/*================Scroll Section===========*/
+
+
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements=document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
+
